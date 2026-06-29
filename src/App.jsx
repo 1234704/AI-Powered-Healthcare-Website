533884b824cfa02156eee094dc44ccf91a06518f
shahryar-dev <shahryarlakho@gmail.com>
2026-06-29 15:16:46 +0500

    feat: initialized React app and completed Day 1 Appointment UI module

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppointmentUI from './components/AppointmentUI';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-50">
+      <AppointmentUI />
+    </div>
+  );
+}
+
+export default App;
